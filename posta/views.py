fd28d3569bc693bb9bc8340aad17440a401e8b86
0LOLUOXH <83889175+0LOLUOXH@users.noreply.github.com>
2025-02-22 22:01:25 -0600

    init autenticacion

--- posta/views.py
+++ posta/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.db import IntegrityError
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+import json
+from .models import *
+
+
+def login_view(request):
+    
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "templates/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "templates/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "templates/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, '', password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "templates/register.html", {
+                "message": "username address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "templates/register.html")
 
 # Create your views here.
